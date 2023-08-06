2abc0d50016acf33ad549eac86ca59d8ffb7d516
sumukhahm2 <sumukhahm2@gmail.com>
2023-08-06 16:51:44 +0530

    first react commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+
+import ExpenseItemList from './ExpenseItemList';
+
+function ExpenseItem()
+{
+    return (
+       <div>
+        <h2>Expense Item!</h2>
+        <ExpenseItemList></ExpenseItemList>
+        </div>
+        );
+    
+}
+
+export default ExpenseItem;
---
+++ src/components/ExpenseItemList.js
@@ -0,0 +1,13 @@
+function ExpenseItemList()
+{
+    return(
+        <div>
+        <h2>Food Rs 10</h2>
+        <h2>Petrol Rs 100</h2>
+        <h2>Movies Rs 200</h2>
+        </div>
+       
+    );
+}
+
+export default ExpenseItemList;
