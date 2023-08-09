523fa0e77e0c3bc416342ce0be6a21d5e0179544
sumukhahm2 <sumukhahm2@gmail.com>
2023-08-09 10:41:14 +0530

    added arrow function and organize component

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetail from './ExpenseDetail';
+import Card from '../UI/card'
+const ExpenseItem=(props)=>
+{
+   
+    return (
+       <Card className='expense-item'>
+       <ExpenseDate date={props.date}/>
+       <ExpenseDetail amount={props.amount} location={props.location} title={props.title}/>
+        </Card>
+        );
+    
+}
+
+export default ExpenseItem;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDom from 'react-dom/client';
 import App from './App';
+import React from 'react'
 const root=ReactDom.createRoot(document.getElementById('root'));
 root.render(<App/>)
