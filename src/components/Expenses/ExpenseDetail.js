65944a170436f7f9a690cd52387e66cfbc4ae175
sumukhahm2 <sumukhahm2@gmail.com>
2023-08-10 14:01:22 +0530

    added event listener update amount and update title

--- src/components/Expenses/ExpenseDetail.js
+++ src/components/Expenses/ExpenseDetail.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './ExpenseDetail.css'
 const ExpenseDetail=(props)=>
 {
-    
+    const [title,updateTitle]=useState(props.title)
+    const [price,priceUpdate]=useState(props.amount)
+    const deleteItem=()=>{
+         updateTitle('updated---')
+    }
+    const updatePrice=()=>{
+        priceUpdate(100)
+    }
     return(
        <div className='expense-item__description'>
-          <h2>{props.title}</h2>
-          <div className='expense-item__price'>${props.amount}</div>
+          <h2>{title}</h2>
+          <div className='expense-item__price'>${price}</div>
+          <button onClick={updatePrice}>update</button>
           <div className='expense-item__location'>{props.location}</div>
+          <button onClick={deleteItem}>Delete</button>
           </div>
     );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,15 +5,12 @@ import ExpenseDetail from './ExpenseDetail';
 import Card from '../UI/card'
 const ExpenseItem=(props)=>
 {
-    const deleteItem=()=>{
-        
-      }
+    
     return (
        <Card className='expense-item'>
        <ExpenseDate date={props.date}/>
        <ExpenseDetail amount={props.amount} location={props.location} title={props.title}/>
-       <button onClick={deleteItem}>Delete</button>
-        </Card>
+       </Card>
         );
     
 }
